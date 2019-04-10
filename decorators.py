1351e284272c28c5fdfd89c6f5d2802f0f367099
Andrii Marusiak <38250529+amarusyak@users.noreply.github.com>
2019-04-10 13:31:37 +0300

    Added singleton decorator and Singleton metaclass

--- decorators.py
+++ decorators.py
@@ -9,3 +9,39 @@ def content_type(method):
             headers['content-type'] = 'application/json'
         return method(headers=headers, *args, **kwargs)
     return wrapper
+
+
+def singleton(class_):
+    """
+    Singleton Decorator.
+    Example of use:
+    >>> @singleton
+    >>> class MyClass(object):
+    >>>     pass
+    NOTE: better use Metaclass...
+    """
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return get_instance
+
+
+class Singleton(type):
+    """
+    Singleton Metaclass.
+    Example of use:
+    >>> class MyClass(metaclass=Singleton):
+    >>>     pass
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        else:
+            # run __init__ every time the class is called
+            cls._instances[cls].__init__(*args, **kwargs)
+        return cls._instances[cls]
